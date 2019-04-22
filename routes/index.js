75fcd79ec07c1bc4541105dbc43fe5a63f3e555b
mustafashaikh4714 <shaikhmustafa.4714@gmail.com>
2019-04-22 23:21:25 +0530

    upload single file and display all the files.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 const upload = require('../utils/mongodbFileUpload')
-module.exports = app => {
+module.exports = (app, gfs) => {
+  // upload single file.
   app.post('/upload', upload.single('file'), (req, res) => {
     res.send({
       success: true,
       message: `file ${req.file} is uploaded successfully`
     })
+  })
+
+  // get all files
+  app.get('/files', async (req, res) => {
+    let files = await gfs.files.find({})
+    if (files.length === 0) {
+      res.status(400).send({ message: ' No files found!' })
+    }
+    return res.send({ files })
   })
 }
